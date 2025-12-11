db411951c12808f584206af9d25257a9491d0ce3
louisalehmann346 <louisalehmann346@gmail.com>
2025-12-10 19:50:11 -0600

    Update register.js

--- frontend/src/pages/register.js
+++ frontend/src/pages/register.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import {Link} from "react-router-dom";
-const apiUrl = "http://localhost:4000/";
+const apiUrl = "https://personalfoodtracker.onrender.com/";
 
 const Register = ({ setUsername }) => {
     const [inputUsername, setInputUsername] = useState("");
